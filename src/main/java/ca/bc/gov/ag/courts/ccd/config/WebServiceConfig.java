b3dd0ad099f366acaa757168130cc9bdf4ce37ad
Shaun Millar <shaun.millar@nttdata.com>
2021-07-29 15:11:18 -0700

    Updated

--- src/main/java/ca/bc/gov/ag/courts/ccd/config/WebServiceConfig.java
+++ src/main/java/ca/bc/gov/ag/courts/ccd/config/WebServiceConfig.java
@@ -16,7 +16,6 @@ import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
 import ca.bc.gov.ag.courts.ccd.utils.CCDwsConstants;
-import ca.bc.gov.ag.courts.ccd.wsdlfix.FixedWsdl11Definition;
 
 /**
  * Web Service Config adapter. 
@@ -37,22 +36,6 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		servlet.setTransformWsdlLocations(true);
 		return new ServletRegistrationBean<>(servlet, "/ws/*");
 	}
-
-//	@Bean(name = "getDocument")
-//	public Wsdl11Definition  defaultWsdl11Definition(XsdSchema getDocumentSchema) {
-//		
-//		//Note the use of the 'Fixed' Wsdl11Def class'. This is required to provide the 'like for like' 
-//		//request structure used by wMethods originally.  
-//		FixedWsdl11Definition wsdl11Definition = new FixedWsdl11Definition();
-//		
-//		wsdl11Definition.setPortTypeName("GetDocumentPort");
-//		wsdl11Definition.setCreateSoap12Binding(true);
-//		wsdl11Definition.setLocationUri("/ws");
-//		wsdl11Definition.setRequestSuffix(""); //must be set to empty string
-//		wsdl11Definition.setTargetNamespace(CCDwsConstants.NAMESPACE_URI);
-//		wsdl11Definition.setSchema(getDocumentSchema);
-//		return wsdl11Definition;
-//	}
 	
 	@Bean(name = "getDocument")
 	public Wsdl11Definition  defaultWsdl11Definition(XsdSchema getDocumentSchema) {
@@ -74,22 +57,6 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	public XsdSchema getDocumentSchema() {
 		return new SimpleXsdSchema(new ClassPathResource("getDocument.xsd"));
 	}
-	
-//	@Bean(name = "getDocumentSecure")
-//	public Wsdl11Definition  defaultWsdl11DefinitionSecure(XsdSchema getDocumentSecureSchema) {
-//		
-//		//Note the use of the 'Fixed' Wsdl11Def class'. This is required to provide the 'like for like' 
-//		//request structure used by wMethods originally.  
-//		FixedWsdl11Definition wsdl11Definition = new FixedWsdl11Definition();
-//		
-//		wsdl11Definition.setPortTypeName("GetDocumentSecurePort");
-//		wsdl11Definition.setCreateSoap12Binding(true);
-//		wsdl11Definition.setLocationUri("/ws");
-//		wsdl11Definition.setRequestSuffix(""); //must be set to empty string
-//		wsdl11Definition.setTargetNamespace(CCDwsConstants.NAMESPACE_URI_SECURE);
-//		wsdl11Definition.setSchema(getDocumentSecureSchema);
-//		return wsdl11Definition;
-//	}
 	
 	@Bean(name = "getDocumentSecure")
 	public Wsdl11Definition  defaultWsdl11DefinitionSecure(XsdSchema getDocumentSecureSchema) {
